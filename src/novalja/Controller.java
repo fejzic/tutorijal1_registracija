f4b4177a1c1f2339ef52ac48cf5a711b42f584f2
dfejzic1 <dfejzic1@etf.unsa.ba>
2020-03-13 12:44:31 +0100

    Initial commit

--- src/novalja/Controller.java
+++ src/novalja/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package novalja;
 
 public class Controller {
 }
--- src/novalja/Main.java
+++ src/novalja/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package novalja;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,12 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
-
-
-public class Main extends Application  {
-
-
+public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
